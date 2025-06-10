ccd728f79db90c215bc8b03ad4a0c799a537cea0
RenzoSV <renzonsv5@gmail.com>
2025-06-09 22:11:19 -0500

    Frontend Destinos2.0

--- AeroLinea/obj/Debug/net8.0/AeroLinea.AssemblyInfo.cs
+++ AeroLinea/obj/Debug/net8.0/AeroLinea.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AeroLinea")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57f02cf8117cb44e0f6d626ebb65df600d917f69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93d026b7b76c27b625bc535360f66d15a17e817e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AeroLinea")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AeroLinea")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
