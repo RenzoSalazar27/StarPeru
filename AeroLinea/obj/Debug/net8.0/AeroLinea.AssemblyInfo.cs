05bd723c37a62fb57affa95b047cf7988fdd4e16
RenzoSV <renzonsv5@gmail.com>
2025-06-09 21:57:15 -0500

    Frontend del home mejorado

--- AeroLinea/obj/Debug/net8.0/AeroLinea.AssemblyInfo.cs
+++ AeroLinea/obj/Debug/net8.0/AeroLinea.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AeroLinea")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+841a1b81a729794a02e51f85ede6865c5aebd811")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+882ba18a1dffaacdf32ba333d0152b0c629a4c30")]
 [assembly: System.Reflection.AssemblyProductAttribute("AeroLinea")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AeroLinea")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
