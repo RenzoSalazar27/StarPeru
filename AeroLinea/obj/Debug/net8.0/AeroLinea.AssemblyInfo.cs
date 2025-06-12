f165eee287eb242f2bd7a9cf536106f6e40fab39
RenzoSV <renzonsv5@gmail.com>
2025-06-11 21:03:58 -0500

    Validaciones en reserva de vuelo

--- AeroLinea/obj/Debug/net8.0/AeroLinea.AssemblyInfo.cs
+++ AeroLinea/obj/Debug/net8.0/AeroLinea.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AeroLinea")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e17e21c72c9307f59becd8007d2c29a307be6980")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12139a44a1544777aac23be97b236c66569c7b64")]
 [assembly: System.Reflection.AssemblyProductAttribute("AeroLinea")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AeroLinea")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
