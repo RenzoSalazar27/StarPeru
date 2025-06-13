76d5d467034c2905d7404f2e335a39a592e8b58d
RenzoSV <renzonsv5@gmail.com>
2025-06-13 01:20:05 -0500

    Mejor posicion para los módulos de voz

--- AeroLinea/obj/Debug/net8.0/AeroLinea.AssemblyInfo.cs
+++ AeroLinea/obj/Debug/net8.0/AeroLinea.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AeroLinea")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+637d95fbd14285b35ff814cc988e49e352e90ac7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3e78478ad6ed4a2d6e7779e79510745572f7ee4")]
 [assembly: System.Reflection.AssemblyProductAttribute("AeroLinea")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AeroLinea")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
