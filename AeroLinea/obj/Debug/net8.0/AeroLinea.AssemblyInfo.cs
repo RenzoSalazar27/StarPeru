cf87f05c3ca820625ff35e0d6b0decf9097e080f
RenzoSV <renzonsv5@gmail.com>
2025-06-21 03:01:31 -0500

    Diseños modernos vista usuario

--- AeroLinea/obj/Debug/net8.0/AeroLinea.AssemblyInfo.cs
+++ AeroLinea/obj/Debug/net8.0/AeroLinea.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AeroLinea")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+005211bc606b18e179dbcedad7893a01a6a42d22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86215964a6e887c6551717831c94d569c77dc2dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("AeroLinea")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AeroLinea")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
