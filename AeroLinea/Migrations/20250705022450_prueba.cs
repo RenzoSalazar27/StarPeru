2f7743d9211833f6df130bd7d0c133f8ccd84361
RenzoSV <renzonsv5@gmail.com>
2025-07-04 21:44:41 -0500

    Diseño a Reserva

---
+++ AeroLinea/Migrations/20250705022450_prueba.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AeroLinea.Migrations
+{
+    /// <inheritdoc />
+    public partial class prueba : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Flota",
+                columns: table => new
+                {
+                    idAvion = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    modeloAvion = table.Column<string>(type: "varchar(15)", maxLength: 15, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    fabricanteAvion = table.Column<string>(type: "varchar(10)", maxLength: 10, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    matriculaAvion = table.Column<string>(type: "varchar(8)", maxLength: 8, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    capacidadAvion = table.Column<int>(type: "int", nullable: false),
+                    claseAvion = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flota", x => x.idAvion);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Pilotos",
+                columns: table => new
+                {
+                    idPiloto = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    nombrePiloto = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    apellidoPiloto = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    nacimientoPiloto = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    telefonoPiloto = table.Column<int>(type: "int", nullable: false),
+                    dniPiloto = table.Column<string>(type: "varchar(8)", maxLength: 8, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    licenciaPiloto = table.Column<string>(type: "varchar(9)", maxLength: 9, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    tipoLicPiloto = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    fechaEmiLic = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pilotos", x => x.idPiloto);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    idUsuario = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    nombresUsuario = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    apellidosUsuario = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    nacimientoUsuario = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    telefonoUsuario = table.Column<string>(type: "varchar(9)", maxLength: 9, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    identificacionUsuario = table.Column<string>(type: "varchar(8)", maxLength: 8, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    correoUsuario = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    contrasenaUsuario = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    esAdmin = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.idUsuario);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Vuelo",
+                columns: table => new
+                {
+                    idVuelo = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    origenVuelo = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    destinoVuelo = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    fechaVuelo = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    idAvion = table.Column<int>(type: "int", nullable: false),
+                    idPiloto = table.Column<int>(type: "int", nullable: false),
+                    precioVuelo = table.Column<decimal>(type: "decimal(6,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vuelo", x => x.idVuelo);
+                    table.ForeignKey(
+                        name: "FK_Vuelo_Flota_idAvion",
+                        column: x => x.idAvion,
+                        principalTable: "Flota",
+                        principalColumn: "idAvion",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Vuelo_Pilotos_idPiloto",
+                        column: x => x.idPiloto,
+                        principalTable: "Pilotos",
+                        principalColumn: "idPiloto",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Consultas",
+                columns: table => new
+                {
+                    idConsulta = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    motivoConsulta = table.Column<string>(type: "varchar(15)", maxLength: 15, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    descripcionConsultas = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    urgencia = table.Column<string>(type: "varchar(10)", maxLength: 10, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    estado = table.Column<string>(type: "varchar(10)", maxLength: 10, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    comentarioConsulta = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    idUsuario = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Consultas", x => x.idConsulta);
+                    table.ForeignKey(
+                        name: "FK_Consultas_Usuarios_idUsuario",
+                        column: x => x.idUsuario,
+                        principalTable: "Usuarios",
+                        principalColumn: "idUsuario",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "reservaVuelos",
+                columns: table => new
+                {
+                    idResVuelo = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    origenResVue = table.Column<string>(type: "varchar(15)", maxLength: 15, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    destinoVuelo = table.Column<string>(type: "varchar(15)", maxLength: 15, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    personasResVue = table.Column<int>(type: "int", nullable: false),
+                    mascotasResVue = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    precioVuelo = table.Column<decimal>(type: "decimal(10,2)", nullable: false),
+                    pagadoVuelo = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    idVuelo = table.Column<int>(type: "int", nullable: false),
+                    idUsuario = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_reservaVuelos", x => x.idResVuelo);
+                    table.ForeignKey(
+                        name: "FK_reservaVuelos_Usuarios_idUsuario",
+                        column: x => x.idUsuario,
+                        principalTable: "Usuarios",
+                        principalColumn: "idUsuario",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_reservaVuelos_Vuelo_idVuelo",
+                        column: x => x.idVuelo,
+                        principalTable: "Vuelo",
+                        principalColumn: "idVuelo",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Pagos",
+                columns: table => new
+                {
+                    IdPago = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    IdReserva = table.Column<int>(type: "int", nullable: false),
+                    Monto = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    Estado = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SessionId = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    FechaPago = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    MetodoPago = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    EmailCliente = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    NombreCliente = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Descripcion = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pagos", x => x.IdPago);
+                    table.ForeignKey(
+                        name: "FK_Pagos_reservaVuelos_IdReserva",
+                        column: x => x.IdReserva,
+                        principalTable: "reservaVuelos",
+                        principalColumn: "idResVuelo",
+                        onDelete: ReferentialAction.Restrict);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Pasajeros",
+                columns: table => new
+                {
+                    idPasajero = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DNI = table.Column<string>(type: "varchar(8)", maxLength: 8, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Nombre = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Edad = table.Column<int>(type: "int", nullable: false),
+                    EsMenorEdad = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    ReservaVueloId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pasajeros", x => x.idPasajero);
+                    table.ForeignKey(
+                        name: "FK_Pasajeros_reservaVuelos_ReservaVueloId",
+                        column: x => x.ReservaVueloId,
+                        principalTable: "reservaVuelos",
+                        principalColumn: "idResVuelo",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Consultas_idUsuario",
+                table: "Consultas",
+                column: "idUsuario");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pagos_IdReserva",
+                table: "Pagos",
+                column: "IdReserva");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pasajeros_ReservaVueloId",
+                table: "Pasajeros",
+                column: "ReservaVueloId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_reservaVuelos_idUsuario",
+                table: "reservaVuelos",
+                column: "idUsuario");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_reservaVuelos_idVuelo",
+                table: "reservaVuelos",
+                column: "idVuelo");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vuelo_idAvion",
+                table: "Vuelo",
+                column: "idAvion");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vuelo_idPiloto",
+                table: "Vuelo",
+                column: "idPiloto");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Consultas");
+
+            migrationBuilder.DropTable(
+                name: "Pagos");
+
+            migrationBuilder.DropTable(
+                name: "Pasajeros");
+
+            migrationBuilder.DropTable(
+                name: "reservaVuelos");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.DropTable(
+                name: "Vuelo");
+
+            migrationBuilder.DropTable(
+                name: "Flota");
+
+            migrationBuilder.DropTable(
+                name: "Pilotos");
+        }
+    }
+}
